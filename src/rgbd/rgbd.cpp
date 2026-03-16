2a9c1ae1c5d85049eaeb14fd87c6f945925a996c
Jonathan Nielsen <jonathan@majj.dk>
2026-03-16 14:11:10 +0100

    working update

--- src/rgbd/rgbd.cpp
+++ src/rgbd/rgbd.cpp
@@ -10,10 +10,8 @@
 
 #ifdef ORBSLAM3_VIZ
 static constexpr int orbslam3_viz_ = ORBSLAM3_VIZ;
-std::cout << "\nORBSLAM3_ROS2 VISUALIZATION: "<< orbslam3_viz_ << std::endl;
 #else
 static constexpr int orbslam3_viz_ = 0; // By default, visualization is off.
-std::cout << "\nORBSLAM3_ROS2 VISUALIZATION OFF BY DEFAULT" << std::endl;
 #endif
 
 int main(int argc, char **argv)
@@ -24,18 +22,16 @@ int main(int argc, char **argv)
         return 1;
     }
 
+    bool visualization = false;
+    if (orbslam3_viz_ == 0){
+        std::cout << "\nORBSLAM3_ROS2 VISUALIZATION OFF BY DEFAULT" << std::endl;
+    }else{
+        std::cout << "\nORBSLAM3_ROS2 VISUALIZATION: "<< orbslam3_viz_ << std::endl;
+        visualization = true;
+        
+    }
+    
     rclcpp::init(argc, argv);
-
-    // malloc error using new.. try shared ptr
-    // Create SLAM system. It initializes all system threads and gets ready to process frames.
-
-    if (orbslam3_viz_ == 1){
-        bool visualization = true;
-
-    }
-    else {
-        bool visualization = false;
-    }
     ORB_SLAM3::System SLAM(argv[1], argv[2], ORB_SLAM3::System::RGBD, visualization);
 
     auto node = std::make_shared<RgbdSlamNode>(&SLAM);
